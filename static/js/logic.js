6409bb84fbd9ccdeccc6fb9560bf7259352c9d04
goodman2814 <mgoodaman@gmail.com>
2021-05-27 19:16:07 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,11 +1,19 @@
-const csv = require('csv-parser');
-const fs = require('fs');
+// const csv = require('csv-parser');
+// const fs = require('fs');
 
-fs.createReadStream('/Users/AlexGoodman/Documents/JHU_Bootcamp/School-Shooting-Analysis/static/data/pah_wikp_combo.csv')
-  .pipe(csv())
-  .on('data', (row) => {
-    console.log(row);
-  })
-  .on('end', () => {
-    console.log('CSV file successfully processed');
-  });
+// let city = []
+// let state = []
+
+// fs.createReadStream('/Users/AlexGoodman/Documents/JHU_Bootcamp/School-Shooting-Analysis/static/data/pah_wikp_combo.csv')
+//   .pipe(csv())
+//   .on('data', (row) => {
+//     city = (row['City']),
+//     state = (row['State']);
+//     console.log(city),
+//     console.log(state)
+//   })
+
+
+let output = ('https://maps.googleapis.com/maps/api/geocode/json?address=1600%20Amphitheatre%20Parkway,
+%20Mountain%20View,%20CA&key='AIzaSyAHdcrKSynTkWgemjldeskYBwVT_M873Ps'')
+console.log(output)
