e3d638057ac2131c3a554a4f964d7ae1161983af
goodman2814 <mgoodaman@gmail.com>
2021-05-27 20:10:41 -0400

    Update logic.js

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,19 +2,19 @@ let city = []
 let state = []
 
 
-var myMap = L.map("map", {
-    center: [45.52, -122.67],
-    zoom: 13
-  });
+// var myMap = L.map("map", {
+//     center: [45.52, -122.67],
+//     zoom: 13
+//   });
 
-L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-  tileSize: 512,
-  maxZoom: 18,
-  zoomOffset: -1,
-  id: "mapbox/streets-v11",
-  accessToken: MAPB_KEY
-}).addTo(myMap);
+// L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//   attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//   tileSize: 512,
+//   maxZoom: 18,
+//   zoomOffset: -1,
+//   id: "mapbox/streets-v11",
+//   accessToken: MAPB_KEY
+// }).addTo(myMap);
 
 
 d3.csv("../static/data/pah_wikp_combo.csv").then(function(data) {
@@ -30,4 +30,5 @@ d3.csv("../static/data/pah_wikp_combo.csv").then(function(data) {
     })
     console.log(city)
     console.log(state);
+    
 });
