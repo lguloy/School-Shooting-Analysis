80a1885d5be0df0cb2047330fe1838aaa222005d
goodman2814 <mgoodaman@gmail.com>
2021-05-28 11:39:56 -0400

    attempting to pull map data
    cleaned up data to get rid of spaces in geocoding requests

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,6 +1,7 @@
 let city = []
 let state = []
 let city_state = []
+let heatArray = []
 
 var myMap = L.map("maps", {
     center: [45.52, -122.67],
@@ -36,25 +37,21 @@ d3.csv("../static/data/overalldata_cleaned.csv").then(function(data) {
         // city_state.push(url.concat(city[i])); 
         var site = `${url}${city[i]},%20${state[i]}&key=${GOOGLE_KEY}`
 
-        d3.json(site).then(function(response) {
-
-            console.log(response);
-          
-            var heatArray = [];
+        d3.json(site).then(function(response) {;
           
             for (var i = 0; i < response.length; i++) {
               var location = response[i].location;
           
               if (location) {
-                heatArray.push([location.coordinates[1], location.coordinates[0]]);
+                heatArray.push([location.location[1], location.location[0]]);
               }
             }
           
-            var heat = L.heatLayer(heatArray, {
-              radius: 20,
-              blur: 35
-            }).addTo(myMap);
+            // var heat = L.heatLayer(heatArray, {
+            //   radius: 20,
+            //   blur: 35
+            // }).addTo(myMap);
         });
-    console.log(site);
+    console.log(heatArray);
     }
 });
